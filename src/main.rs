80d6b4c615d27d0499e3a2a1449fe9e53ac62e4d
Simon Danielsson <116196524+simon-danielsson@users.noreply.github.com>
2026-02-10 12:10:41 +0100

    random word generation, error counter and stateful validation

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::{
+    collections::HashSet,
     io::{self, Stdout, Write, stdout},
     thread,
     time::Duration,
@@ -9,12 +10,16 @@ use crossterm::{
     cursor::MoveTo,
     style::{Color, ResetColor, SetBackgroundColor, SetForegroundColor},
 };
+use rand::seq::IndexedMutRandom;
 
 mod controls;
 mod utils;
 
+const WORDS: &str = include_str!("words.txt");
 const ERROR_BG: Color = Color::Red;
 const ERROR_FG: Color = Color::White;
+
+const SENT_LEN: i32 = 10;
 
 const USER_ENTRY_HL: Color = Color::Blue;
 const FPS: f64 = 100.0;
@@ -63,10 +68,11 @@ struct Tecken {
     state: State,
     fps: Duration,
 
+    word_pool: Vec<String>,
     current_sentence: String,
     text_entry_buff: String,
     user_typing_errors: i32,
-    invalid_letters_col_pos: Vec<u16>,
+    invalid_letters_col_pos: HashSet<u16>,
 }
 
 impl Tecken {
@@ -78,11 +84,25 @@ impl Tecken {
             state: State::Main,
             fps: utils::get_fps(FPS),
 
-            current_sentence: String::from("this is a sentence"),
+            word_pool: Vec::new(),
+            current_sentence: String::new(),
             text_entry_buff: String::new(),
             user_typing_errors: 0,
-            invalid_letters_col_pos: Vec::new(),
+            invalid_letters_col_pos: HashSet::new(),
         }
+    }
+
+    fn gen_new_sentence(&mut self) {
+        let mut rng = rand::rng();
+        let mut sentence = String::new();
+        for _ in 0..SENT_LEN {
+            if let Some(word) = self.word_pool.choose_mut(&mut rng) {
+                sentence.push_str(word);
+                sentence.push(' ');
+            }
+        }
+        sentence.pop();
+        self.current_sentence = sentence;
     }
 
     fn write_word(&mut self, s: String) -> io::Result<()> {
@@ -108,14 +128,20 @@ impl Tecken {
     fn validation(&mut self) -> io::Result<()> {
         let exercise_chars: Vec<char> = self.current_sentence.chars().collect();
         let user_chars: Vec<char> = self.text_entry_buff.chars().collect();
-        self.invalid_letters_col_pos.clear();
-        self.user_typing_errors = 0;
+
+        let mut new_invalids = HashSet::new();
+
         for (i, (e_ch, u_ch)) in exercise_chars.iter().zip(user_chars.iter()).enumerate() {
             if e_ch != u_ch {
-                self.user_typing_errors += 1;
-                self.invalid_letters_col_pos.push(i as u16);
+                new_invalids.insert(i as u16);
             }
         }
+
+        for _ in new_invalids.difference(&self.invalid_letters_col_pos) {
+            self.user_typing_errors += 1;
+        }
+
+        self.invalid_letters_col_pos = new_invalids;
         Ok(())
     }
 
@@ -140,6 +166,13 @@ impl Tecken {
         Ok(())
     }
 
+    fn write_error_amt(&mut self) -> io::Result<()> {
+        self.sout.queue(MoveTo(0, 1))?;
+        let s = format!("Errors: {}", self.user_typing_errors);
+        self.sout.write(s.as_bytes())?;
+        Ok(())
+    }
+
     fn main_loop(&mut self) -> io::Result<()> {
         self.clear_screen()?;
         // write exercise word
@@ -149,6 +182,15 @@ impl Tecken {
         // check for errors in input
         self.validation()?;
         self.write_errors()?;
+
+        self.write_error_amt()?;
+
+        // if sentence is finished, generate new one
+        if self.text_entry_buff.chars().count() == self.current_sentence.chars().count() {
+            self.text_entry_buff.clear();
+            self.current_sentence.clear();
+            self.gen_new_sentence();
+        }
         Ok(())
     }
 }
--- src/utils.rs
+++ src/utils.rs
@@ -1,14 +1,14 @@
 use std::{io, time::Duration};
 
 use crossterm::{
-    ExecutableCommand, QueueableCommand,
+    ExecutableCommand, QueueableCommand, cursor,
     terminal::{
         self, Clear, ClearType, EnterAlternateScreen, LeaveAlternateScreen,
         disable_raw_mode, enable_raw_mode,
     },
 };
 
-use crate::Tecken;
+use crate::{Tecken, WORDS};
 
 impl Tecken {
     pub fn setup(&mut self) -> io::Result<()> {
@@ -16,12 +16,20 @@ impl Tecken {
         (self.columns, self.rows) = terminal::size()?;
         enable_raw_mode()?;
         self.clear_screen()?;
+        self.gen_word_pool();
+        self.sout.queue(cursor::SavePosition)?;
+        self.sout.queue(cursor::Hide)?;
         Ok(())
+    }
+    pub fn gen_word_pool(&mut self) {
+        self.word_pool = WORDS.split_whitespace().map(String::from).collect();
     }
 
     pub fn quit_cleanup(&mut self) -> io::Result<()> {
         disable_raw_mode()?;
         self.sout.execute(LeaveAlternateScreen)?;
+        self.sout.queue(cursor::RestorePosition)?;
+        self.sout.queue(cursor::Show)?;
         Ok(())
     }
 
